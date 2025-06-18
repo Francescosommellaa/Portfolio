a4f9ca39a7a7bb98835087bf18e1795baaebd912
Francescosommellaa <francescosommellaa@gmail.com>
2025-06-18 15:51:06 +0200

    fix: resolve alias with __dirname for Vercel deploy

--- francesco/vite.config.ts
+++ francesco/vite.config.ts
@@ -1,12 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
   base: "/",
   resolve: {
     alias: {
-      '@styles': '/src/styles',
+      '@styles': resolve(__dirname, 'src/styles'), // ✅ ora funziona ovunque
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@styles/variables" as *;
+          @use "@styles/mixins" as *;
+          @use "@styles/typography" as *;
+        `,
+      },
     },
   },
 })
